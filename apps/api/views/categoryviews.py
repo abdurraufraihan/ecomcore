efcc77d056922383dc49c8278d8121eae5cc5cd0
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-29 01:57:30 +0600

    internal server error added for category list api

--- apps/api/views/categoryviews.py
+++ apps/api/views/categoryviews.py
@@ -1,7 +1,14 @@
 from rest_framework.generics import ListAPIView
+from lib import errorutility as errorUtil
 from apps.product.models import Category
 from apps.product.serializers.categoryserializer import CategoryListSerializer
 
 class CategoryListView(ListAPIView):
 	queryset = Category.objects.all()
 	serializer_class = CategoryListSerializer
+
+	def get(self, request, *args, **kwargs):
+		try:
+			return self.list(request, *args, **kwargs)
+		except:
+			return errorUtil.getInternalServerErrorResponse()
