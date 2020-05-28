ca73658e0cb9b48644d25b08fc5c0e53fd5f2257
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-29 00:37:36 +0600

    added product filter according to category id

--- apps/api/views/productviews.py
+++ apps/api/views/productviews.py
@@ -1,7 +1,16 @@
 from rest_framework.generics import ListAPIView
+from lib import constants as const
 from apps.product.models import Product
 from apps.product.serializers.productserializer import ProductSerializer
 
 class ProductListView(ListAPIView):
 	serializer_class = ProductSerializer
-	queryset = Product.objects.all()
+
+	def get_queryset(self):
+		categoryId = \
+			self.request.query_params.get(const.CATEGORY_ID_QUERY_PARAM)
+		productFilter = {}
+		if categoryId:
+			productFilter[const.CATEGORY_ID_FILTER_PROPERTY] = categoryId
+		queryset = Product.objects.filter(**productFilter)
+		return queryset
