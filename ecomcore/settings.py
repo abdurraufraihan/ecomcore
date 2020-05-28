18a1859d9b19558f72d180a1cee51ea3cca33e19
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 22:33:42 +0600

    api app added

--- ecomcore/settings.py
+++ ecomcore/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.product'
+    'apps.product',
+    'apps.api'
 ]
 
 MIDDLEWARE = [
