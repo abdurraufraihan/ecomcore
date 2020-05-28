bcf050c3c2c3d1f4811e9e6cf21851dc6fb33230
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 16:50:37 +0600

    category model added

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,6 +1,15 @@
 import uuid
 from django.db import models
 
+class Category(models.Model):
+	categoryId = models.UUIDField(
+		default=uuid.uuid4, editable=False, unique=True
+	)
+	name = models.CharField(max_length=30)
+
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
 	productId = models.UUIDField(
 		default=uuid.uuid4, editable=False, unique=True
@@ -9,6 +18,9 @@ class Product(models.Model):
 	description = models.TextField()
 	price = models.FloatField()
 	image = models.ImageField(upload_to='product/')
+	category = models.ForeignKey(
+		Category, on_delete=models.SET_NULL, null=True, blank=True
+	)
 
 	def __str__(self):
 		return self.title
