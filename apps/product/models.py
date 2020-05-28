4fe160058be46d68e8521b5f2ebac43c54c9e9f5
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 17:02:54 +0600

    added constants for category max length and product upload path

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,11 +1,12 @@
 import uuid
 from django.db import models
+from lib import constants
 
 class Category(models.Model):
 	categoryId = models.UUIDField(
 		default=uuid.uuid4, editable=False, unique=True
 	)
-	name = models.CharField(max_length=30)
+	name = models.CharField(max_length=constants.CATEGORY_NAME_MAX_LENGTH)
 
 	def __str__(self):
 		return self.name
@@ -17,7 +18,7 @@ class Product(models.Model):
 	title = models.CharField(max_length=50)
 	description = models.TextField()
 	price = models.FloatField()
-	image = models.ImageField(upload_to='product/')
+	image = models.ImageField(upload_to=constants.PRODUCT_IMAGE_UPLOAD_PATH)
 	category = models.ForeignKey(
 		Category, on_delete=models.SET_NULL, null=True, blank=True
 	)
