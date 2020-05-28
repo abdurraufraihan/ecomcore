ec667b79c11bc3671f8ecf2e7e4c72c914033a43
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-29 00:44:58 +0600

    added product search filter

--- apps/api/views/productviews.py
+++ apps/api/views/productviews.py
@@ -9,8 +9,11 @@ class ProductListView(ListAPIView):
 	def get_queryset(self):
 		categoryId = \
 			self.request.query_params.get(const.CATEGORY_ID_QUERY_PARAM)
+		search = self.request.query_params.get(const.SEARCH_QUERY_PARAM)
 		productFilter = {}
 		if categoryId:
 			productFilter[const.CATEGORY_ID_FILTER_PROPERTY] = categoryId
+		if search:
+			productFilter[const.TITLE_CONTAINS_FILTER_PROPERTY] = search
 		queryset = Product.objects.filter(**productFilter)
 		return queryset
