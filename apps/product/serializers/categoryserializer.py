9ef562db2f4ab01f371aeb028cfe58ea33104278
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 22:30:24 +0600

    category serializer created

---
+++ apps/product/serializers/categoryserializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from lib import constants as const
+from apps.product.models import Category
+
+class CategorySerializer(serializers.ModelSerializer):
+	id = serializers.UUIDField(source=const.CATEGORY_ID_PROPERTY)
+
+	class Meta:
+		model = Category
+		fields = [const.ID_PROPERTY, const.NAME_PROPERTY]
