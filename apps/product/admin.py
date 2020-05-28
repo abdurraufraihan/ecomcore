524378a233456b7529a804037334d47b67f8e947
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 17:18:28 +0600

    added product and category to admin

--- apps/product/admin.py
+++ apps/product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from lib import constants as const
+from apps.product.models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+	list_display = [
+		const.ID_PROPERTY,
+		const.PRODUCT_ID_PROPERTY,
+		const.TITLE_PROPERTY,
+		const.DESCRIPTION_PROPERTY,
+		const.PRICE_PROPERTY,
+		const.IMAGE_PROPERTY,
+		const.CATEGORY_PROPERTY
+	]
+
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = [
+		const.ID_PROPERTY,
+		const.CATEGORY_ID_PROPERTY,
+		const.NAME_PROPERTY
+	]
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
