2d7bb6a8db7a5e54f65bccbd6ec1b03a93b525f5
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-05-28 16:48:38 +0600

    product model implemented

--- apps/product/models.py
+++ apps/product/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	productId = models.UUIDField(
+		default=uuid.uuid4, editable=False, unique=True
+	)
+	title = models.CharField(max_length=50)
+	description = models.TextField()
+	price = models.FloatField()
+	image = models.ImageField(upload_to='product/')
+
+	def __str__(self):
+		return self.title
